98b2a965b7239628cefb106641087a79bf3f4b08
paulochiaradia <paulo.chiaradia@outlook.com>
2023-08-08 15:44:21 -0300

    SAVE 1

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -14,3 +14,29 @@ func TestIfGetsAnErrorIfPriceIsBlank(t *testing.T) {
 	order := Order{ID: "test"}
 	assert.Error(t, order.Validate(), "price need to be greater than zero")
 }
+
+func TestIfGetsAnErrorIfTaxIsBlank(t *testing.T) {
+	order := Order{
+		ID:    "test",
+		Price: 10,
+	}
+	assert.Error(t, order.Validate(), "invalid tax")
+}
+
+func TestFinalPrice(t *testing.T) {
+	order := Order{
+		ID:    "132",
+		Price: 10,
+		Tax:   1,
+	}
+	assert.NoError(t, order.Validate())
+	assert.Equal(t, "132", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+	err := order.CalculateFinalPrice()
+	if err != nil {
+		return
+	}
+	assert.Equal(t, 11.0, order.FinalPrice)
+
+}
